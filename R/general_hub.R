f78fc29d70e3ca3d4027bb5516e23b67555bffd5
Chantelle Hoang <chantelle.hoang@students.mq.edu.au>
2024-09-02 14:45:05 +1000

    Initial commit

---
+++ R/general_hub.R
@@ -0,0 +1,16 @@
+usethis::use_pipe()
+devtools::document()
+
+usethis::use_r("say")
+
+say()
+
+check()
+
+usethis::use_mit_license("Chantelle Hoang")
+
+usethis::use_testthat()
+
+usethis::use_test("Test-say")
+
+devtools::test_coverage()
